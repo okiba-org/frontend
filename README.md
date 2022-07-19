fd209174094b40a6f7f2a7b393fe07b26efc8b58
alok8bb <alok8bb@gmail.com>
2022-07-19 23:08:16 +0530

    feat: add lazy loading app, readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Okiba
+
+This repository contains okiba bin's front-end code. The web app is using codemirror editor, react library with vite bundler.
+
+## Usage
+
+```shell
+// get deps
+$ pnpm install
+
+// build
+$ pnpm run build
+
+// dev server
+$ pnpm run dev
+
+// preview
+$ pnpm run preview
+```
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+
+const LazyApp = React.lazy(() => import("./App"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<Suspense fallback={"Loading..."}>
+			<LazyApp />
+		</Suspense>
 	</React.StrictMode>,
 );
